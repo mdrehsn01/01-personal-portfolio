fd6566fa49834511ddcd5a6ebafdbdf26b581034
Rehan <mohammadrehan202121@gmail.com>
2026-09-06 21:21:16 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">
+        Personal Portfolio
+      </h1>
+
+      <button className="btn btn-dark">
+        Test Button
+      </button>
+    </div>
+  )
+}
+
+export default App
